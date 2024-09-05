c4954424521aa9b3e8fa8cab38152bd22c18240f
teakdemir <136896802+teakdemir@users.noreply.github.com>
2024-09-05 13:21:24 +0300

    basicfunctionlogic

---
+++ def_main.py
@@ -0,0 +1,13 @@
+def main():
+    x =int(input("What's x? "))
+    print("x squared is ", square(x))
+
+
+def square(n):
+    return n*n
+
+
+main()
+
+
+#dont forget to hit the tab after def : (for every nw line)
