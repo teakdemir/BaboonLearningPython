996e0b146e5bbbcb7738cd27e4a66d3757282756
teakdemir <136896802+teakdemir@users.noreply.github.com>
2024-09-05 13:17:16 +0300

    ugicibugici

---
+++ funcdef.py
@@ -0,0 +1,11 @@
+def hello(to="World"):
+    print("Hello",to)
+
+name =input("What's your name? ")
+
+#we are gonna create our own function named hello using def for define
+#using "to", so that we can pass the parameter "name". name copied to "to"
+#you can give default values to the parameters. to="world" just in case we user does not enter.
+
+hello(name)
+
